818f4a3847032a16e449a9358cf9442538e9f47a
Yao Liu <xuanyao0111@gmail.com>
2013-09-06 00:16:04 +0800

    first commit

---
+++ Mapper/build/src/CMakeFiles/mapper.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/mapper.dir/main.cpp.o"
+  "mapper.pdb"
+  "mapper"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/mapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
